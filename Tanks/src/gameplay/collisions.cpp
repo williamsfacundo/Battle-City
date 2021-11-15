f88b8157e33b0d48f2b8062acf9bbd57b60b8eb3
facundo <facuwilli@gmail.com>
2021-11-15 16:21:03 -0300

    Add rectangle collision detection function.

--- Tanks/src/gameplay/collisions.cpp
+++ Tanks/src/gameplay/collisions.cpp
@@ -4,6 +4,11 @@ namespace Battle_City
 {
 	namespace CollisionFunctions
 	{
-
+		bool CollisionRectangles(float x1, float y1, float width1, float height1,
+			float x2, float y2, float width2, float height2)
+		{
+			return (x1 < x2 + width2 && x1 + width1 > x2 &&
+				y1 < y2 + height2 && y1 + height1 > y2);
+		}
 	}
 }
--- Tanks/src/gameplay/collisions.h
+++ Tanks/src/gameplay/collisions.h
@@ -1,11 +1,12 @@
 #ifndef COLLISIONS_H
-#define COLLISIONS_H
+#define COLLISIONS_H 
 
 namespace Battle_City
 {
 	namespace CollisionFunctions
 	{
-				
+		bool CollisionRectangles(float x1, float y1, float width1, float height1, 
+			float x2, float y2, float width2, float height2);
 	}
 }
 
